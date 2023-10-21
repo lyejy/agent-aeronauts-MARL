f9c1685b0f56b7ac9bae9d4a60911346f70b6e65
SARAH ANN HOGAN _ <sahogan.2020@smu.edu.sg>
2023-10-22 00:17:52 +0800

    Updated the logic for capacity constraints and using timeslots instead of movement

--- MILP_Solver.py
+++ MILP_Solver.py
@@ -1,30 +1,65 @@
 import pulp
+import random
+
 
 # Parameters
 num_requests = 15000
-num_timeslots = 288
+num_timeslots_per_day = 288
+num_days = 182 
+total_timeslots = num_requests * num_timeslots_per_day
 capacity = 10  # Fixed capacity for each timeslot
 movements = 5  # Maximum allowed distance from the original timeslot
+
+
+# Constants for original arrival and departure slots
+original_arrival = {i: random.randint(0, total_timeslots - 1) for i in range(num_requests)}
+original_departure = {i: (original_arrival[i] + 7) % total_timeslots for i in range(num_requests)}
+
 
 # Create a PuLP problem
 problem = pulp.LpProblem("FlightAssignment", pulp.LpMinimize)
 
-# Binary decision variables: m[i, j] is 1 if request i is moved to timeslot j
-m = pulp.LpVariable.dicts("Movement", ((i, j) for i in range(num_requests) for j in range(movements)), cat=pulp.LpBinary)
+# Binary decision variables: 
+# arrival[i, j] is 1 if request i arrives at timeslot j
+# departure[i, j] is 1 if request i departs from timeslot j
+arrival = pulp.LpVariable.dicts("Arrival", ((i, j) for i in range(num_requests) for j in range(total_timeslots)), cat=pulp.LpBinary)
+departure = pulp.LpVariable.dicts("Departure", ((i, j) for i in range(num_requests) for j in range(total_timeslots)), cat=pulp.LpBinary)
 
 # Constraints
-# Each request is assigned to exactly one timeslot
+# Each request arrives at exactly one timeslot
 for i in range(num_requests):
-    problem += pulp.lpSum(m[i, j] for j in range(num_timeslots)) == 1
+    problem += pulp.lpSum(arrival[i, j] for j in range(total_timeslots)) == 1
+
+# Each request departs from exactly one timeslot
+for i in range(num_requests):
+    problem += pulp.lpSum(departure[i, j] for j in range(total_timeslots)) == 1
 
 # Capacity constraint for each timeslot
-for j in range(num_timeslots):
-    problem += pulp.lpSum(m[i, j] for i in range(num_requests)) <= capacity
+for j in range(total_timeslots):
+    problem += pulp.lpSum(arrival[i, j] for i in range(num_requests)) + pulp.lpSum(departure[i, j] for i in range(num_requests)) <= capacity
 
-# Additional constraint: ensure that the new timeslot is within the max number of moves allowed 
+# Ensure that the departure and arrival timeslots for a flight request are not the same 
 for i in range(num_requests):
-    for j in range(num_timeslots):
-        problem += m[i, j] * abs(i - j) <= movements//2 
+    for j in range(total_timeslots):
+        problem += arrival[i, j] + departure[i, j] <= 1  # A request can't arrive and depart from the same timeslot
+
+# Ensure that the updated slot is at most a certain number of slots away  
+for i in range(num_requests):
+    for j in range(total_timeslots):
+        problem += arrival[i, j] * abs(original_arrival[i] - j) <= movements//2 
+
+for i in range(num_requests):
+    for j in range(total_timeslots):
+        problem += departure[i, j] * abs(original_departure[i] - j) <= movements//2 
+
+
+#Minimize the number of timeslots between the original timeslot and the updated timeslot 
+problem += pulp.lpSum(
+    arrival[i, j] * abs(original_arrival[i] - j) + departure[i, j] * abs(original_departure[i] - j)
+    for i in range(num_requests)
+    for j in range(total_timeslots)
+)
+
 
 # Solve the problem
 problem.solve()
@@ -32,12 +67,12 @@ problem.solve()
 if problem.status == pulp.LpStatusOptimal:
     # Print the optimal assignments
     for i in range(num_requests):
-        for j in range(movements):
-            if pulp.value(m[i, j]) == 1:
-                print(f"Request {i} moved {j} number of slots")
+        arrival_slot = next(j for j in range(total_timeslots) if pulp.value(arrival[i, j]) == 1)
+        departure_slot = next(j for j in range(total_timeslots) if pulp.value(departure[i, j]) == 1)
+        print(f"Request {i} arrives at timeslot {arrival_slot} and departs from timeslot {departure_slot}")
 
     # Print the total distance
-    total_distance = sum(pulp.value(m[i, j]) * abs(i - j) for i in range(num_requests) for j in range(movements))
+    total_distance = sum(abs(arrival_slot - departure_slot) for i in range(num_requests))
     print(f"Total distance: {total_distance}")
 else:
     # Print number of remaining requests violating constraints 
